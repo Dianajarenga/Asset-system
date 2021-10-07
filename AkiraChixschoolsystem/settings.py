5f697317ab69a4e19fa1a9f021ed2c0686bc5711
Dianajarenga <jarengadiana@gmail.com>
2021-10-07 15:48:36 +0300

    correction

--- AkiraChixschoolsystem/settings.py
+++ AkiraChixschoolsystem/settings.py
@@ -87,7 +87,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static',]
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 

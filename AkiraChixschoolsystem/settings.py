6894f6beab06163abafe1eee91acc2086b6a096e
Dianajarenga <jarengadiana@gmail.com>
2021-10-07 15:53:07 +0300

    correction

--- AkiraChixschoolsystem/settings.py
+++ AkiraChixschoolsystem/settings.py
@@ -84,10 +84,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'AkiraChixschoolsystem.wsgi.application'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static',]
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT =os.path.join(BASE_DIR,'media')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
